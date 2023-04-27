019af323855c77b38cfeeb1c36b7fd355379459b
Krishna <vasanikrishna19@gmail.com>
2023-04-27 22:58:19 +0100

    SeleniumBasics Added

--- src/test/java/org/example/Next.java
+++ src/test/java/org/example/Next.java
@@ -17,11 +17,12 @@ public class Next {
         driver1.manage().window().maximize();
     }
     @Test
-    public void openNextUrl(){
-        driver1.get("");
+    public void openNextUrl() throws InterruptedException {
+        driver1.get("https://www.next.co.uk/");
+        Thread.sleep(3000);
         driver1.findElement(By.id("onetrust-accept-btn-handler")).click();
+        driver1.findElement(By.xpath("/html/body/div[2]/div/section/header/div[1]/nav/div[2]/div[2]/div[2]/div/a")).click();
     }
-
     @After
     public void tearDown(){
         driver1.close();
